6d4ce6f75559604011ffa964d76205547db1e534
Alex Harvey <alexharv074@gmail.com>
2018-08-10 12:03:26 +1000

    Simplify the tests

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source 'https://rubygems.org'
 group :tests do
   gem 'rspec', :require => false
   gem 'rake',  :require => false
-  gem 'liquid', :require => false
 end
 
 gem 'pry'
--- spec/validate_spec.rb
+++ spec/validate_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
-require 'liquid'
 
 describe 'posts' do
   Dir.glob("_posts/*md").each do |file|
     context file do
       context 'front matter' do
         it 'date should match filename' do
-          date_in_file_name  = file.gsub(/_posts\/(\d{4}-\d{2}-\d{2})-.*/, '\1')
-          template = Liquid::Template.parse(File.read(file))
-          date_in_file_content = template.render.split(/---/)[1].lines.select{ |x| x =~ /^date/ }[0].chomp.split[1]
+
+          date_in_file_name =
+            file.gsub(/_posts\/(\d{4}-\d{2}-\d{2})-.*/, '\1')
+
+          date_in_file_content =
+            File.read(file).split(/---/)[1].lines.select{ |x| x =~ /^date/ }[0].chomp.split[1]
+
           expect(date_in_file_name).to eq date_in_file_content
         end
       end
