d617a7ec9f8953f209fb36d3fab7e2f6a7989268
Alex Harvey <alexharv074@gmail.com>
2018-08-11 22:46:25 +1000

    Correction to tests

--- spec/validate_spec.rb
+++ spec/validate_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 require 'yaml'
-require 'time'
 
 # Documented at https://jekyllrb.com/news/2017/03/02/jekyll-3-4-1-released/
 post_regex = %r!^(?:.+/)*(\d{2,4}-\d{1,2}-\d{1,2})-(.*)(\.[^.]+)$!
@@ -9,18 +8,18 @@ describe 'posts' do
   Dir.glob("_posts/*md").each do |file|
     basename = File.basename(file)
 
-    front_matter = YAML.load(File.read(file).split(/---/)[1])
-
-    date_in_file_name =
-      Time.parse(post_regex.match(basename).captures[0]).to_date
-
     context basename do
+      front_matter = YAML.load(File.read(file).split(/---/)[1])
+
       it 'filename must match documented post regex' do
         expect(basename).to match post_regex
       end
 
       it 'date in file name same day as date in front matter' do
-        expect(front_matter['date']).to be === date_in_file_name
+        date_in_file_name = Date.parse(post_regex.match(basename).captures[0])
+        date_in_front_matter = front_matter['date'].to_date
+
+        expect(date_in_front_matter).to eq date_in_file_name
       end
 
       it 'title in front matter should not contain a colon' do
