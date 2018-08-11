9abfef7d87c243d1b2b28015f7d20d6e1dcc6021
Alex Harvey <alexharv074@gmail.com>
2018-08-11 23:45:29 +1000

    Yes more changes

--- spec/validate_spec.rb
+++ spec/validate_spec.rb
@@ -1,8 +1,15 @@
+
 require 'spec_helper'
 require 'yaml'
 
 # Documented at https://jekyllrb.com/news/2017/03/02/jekyll-3-4-1-released/
 post_regex = %r!^(?:.+/)*(\d{2,4}-\d{1,2}-\d{1,2})-(.*)(\.[^.]+)$!
+
+def date_in_front_matter(date)
+  return date if date.is_a?(Date)
+  return date.to_date if date.is_a?(Time)
+  return Date.parse(date) if date.is_a?(String)
+end
 
 describe 'posts' do
   Dir.glob("_posts/*md").each do |file|
@@ -15,10 +22,15 @@ describe 'posts' do
         expect(basename).to match post_regex
       end
 
-      it 'date in file name same day as date in front matter' do
-        date_in_file_name = Date.parse(post_regex.match(basename).captures[0])
-        date_in_front_matter = front_matter['date'].to_date
+      date_string = post_regex.match(basename).captures[0]
 
+      it 'date in file name should be a valid date' do
+        expect { Date.parse(date_string) }.to_not raise_error
+      end
+
+      it 'date in file name should be same day as in front matter' do
+        date_in_file_name = Date.parse(date_string)
+        date_in_front_matter = date_in_front_matter(front_matter['date'])
         expect(date_in_front_matter).to eq date_in_file_name
       end
 
