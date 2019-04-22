c02850eb69c29fc99880b8943a925a2aa8b5e206
Alex Harvey <alexharv074@gmail.com>
2019-04-22 21:18:17 +1000

    Add remaining unit tests for cookbook

--- shunit2/sed_and_awk_cookbook.sh
+++ shunit2/sed_and_awk_cookbook.sh
@@ -45,24 +45,147 @@ qux" > /tmp/EXPECTED
   assertEquals "$(</tmp/EXPECTED)" "$(</tmp/FILE)"
 }
 
-# sed -i '1h; 1!H; $!d; x; s/.*PATTERN[^\n]*/&\nfoo/' FILE
-# sed -n '/PATTERN/{n;p;}'
-# sed -n '/PATTERN/{n;n;p;}'
-# sed '$!N; /PATTERN/P; D'
-# sed '1N; $!N; /.*\n.*\n.*PATTERN.*/P; D'
-# sed '1{N;N};$!N;/.*\n.*\n.*\n.*pattern/P;D'
-# sed '1{N;N;N}; $!N; /.*\n.*\n.*\n.*\n.*PATTERN.*/P; D'
-# sed -n 11p
-# sed -n 4,11p
-# sed -n '4,$p'
-# - Solution using sed
-# sed -n '/PATTERN1/,/PATTERN2/p'
-# - Solution using sed
-# sed -n '/PATTERN1/,/PATTERN2/p;/PATTERN2/q'
-# - Solution using sed
-# sed -n '/PATTERN1/,/PATTERN2/{//!p;}'
-# - Solution using GNU sed
-# gsed '0,/PATTERN1/d;/PATTERN2/Q'
-# gsed -i 's/  *$//' FILE
+testInsertAfterLastInstanceOfPattern() {
+  echo "aaa
+bbb
+ccc
+bbb
+ccc
+eee" > /tmp/FILE
+  echo "aaa
+bbb
+ccc
+bbb
+ccc
+ddd
+eee" > /tmp/EXPECTED
+  sed -i '1h; 1!H; $!d; x; s/.*ccc[^\n]*/&\nddd/' /tmp/FILE
+  assertEquals "$(</tmp/EXPECTED)" "$(</tmp/FILE)"
+}
+
+testPrintLineAfterPattern() {
+  echo "aaa
+bbb
+ccc
+bbb
+ccc
+eee" > /tmp/FILE
+  output=$(sed -n '/bbb/{n;p}' /tmp/FILE)
+  expected="ccc
+ccc"
+  assertEquals "$expected" "$output"
+}
+
+testPrintTwoLinesAfterPattern() {
+  echo "aaa
+bbb
+ccc
+aaa
+bbb
+ccc" > /tmp/FILE
+  output=$(sed -n '/aaa/{n;n;p}' /tmp/FILE)
+  expected="ccc
+ccc"
+  assertEquals "$expected" "$output"
+}
+
+testPrintLineBeforePattern() {
+  echo "aaa
+bbb
+ccc
+aaa
+bbb
+ccc" > /tmp/FILE
+  output=$(sed '$!N; /.*\n.*bbb/P; D' /tmp/FILE)
+  expected="aaa
+aaa"
+  assertEquals "$expected" "$output"
+}
+
+testPrintTwolinesBeforePattern() {
+  echo "aaa
+bbb
+ccc
+aaa
+bbb
+ccc" > /tmp/FILE
+  output=$(sed '1N; $!N; /.*\n.*\n.*ccc/P; D' /tmp/FILE)
+  expected="aaa
+aaa"
+  assertEquals "$expected" "$output"
+}
+
+testPrintFourLinesBeforePattern() {
+  echo "aaa
+bbb
+ccc
+ddd
+eee
+aaa
+bbb
+ccc
+ddd
+eee" > /tmp/FILE
+  output=$(sed '1{N;N;N}; $!N; /.*\n.*\n.*\n.*\n.*eee/P; D' /tmp/FILE)
+  expected="aaa
+aaa"
+  assertEquals "$expected" "$output"
+}
+
+testPrintLinesBetweenPatternsInclusive() {
+  echo "aaa
+PAT1
+bbb
+ccc
+ddd
+PAT2
+eee
+PAT1
+fff" > /tmp/FILE
+  output=$(sed -n '/PAT1/,/PAT2/p' /tmp/FILE)
+  expected="PAT1
+bbb
+ccc
+ddd
+PAT2
+PAT1
+fff"
+  assertEquals "$expected" "$output"
+}
+
+testPrintLinesBetweenPatternsExclusive() {
+  echo "aaa
+PAT1
+bbb
+ccc
+ddd
+PAT2
+eee
+PAT1
+fff" > /tmp/FILE
+  output=$(sed -n '/PAT1/,/PAT2/{//!p}' /tmp/FILE)
+  expected="bbb
+ccc
+ddd
+fff"
+  assertEquals "$expected" "$output"
+}
+
+testPrintLinesBetweenPatternsExclusiveFirstOnly() {
+  echo "aaa
+PAT1
+bbb
+ccc
+ddd
+PAT2
+eee
+PAT1
+fff" > /tmp/FILE
+  output=$(sed '0,/PAT1/d;/PAT2/Q' /tmp/FILE)
+  expected="bbb
+ccc
+ddd"
+  assertEquals "$expected" "$output"
+}
 
 . shunit2
